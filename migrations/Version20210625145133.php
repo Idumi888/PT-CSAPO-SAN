084ccf26edaf464a942bc9ab95c42a8ec2700123
Idumi888 <vincent.dievart@epsi.fr>
2021-06-27 18:39:41 +0000

    apiressource users

---
+++ migrations/Version20210625145133.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210625145133 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE epreuve ADD heure_debut_epreuve DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE passe ADD date_rendu_epreuve DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE epreuve DROP heure_debut_epreuve');
+        $this->addSql('ALTER TABLE passe DROP date_rendu_epreuve');
+    }
+}
